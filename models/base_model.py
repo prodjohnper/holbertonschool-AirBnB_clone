8c6be2ce7c759077a3cf3e764a9491ab995f8062
Louis Toro <louistororosario@gmail.com>
2024-02-19 21:29:17 -0400

    BaseModel class with required attributes and methods

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,25 @@ import datetime
 
 class BaseModel:
     def __init__(self):
-        id = uuid.uuid4()
-        self.id = str(id)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
-obj = BaseModel()
-print(obj.id)
-print(obj.created_at)
-print(obj.updated_at)
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
+
+# obj = BaseModel()
+# print(obj.id)
+# print(obj.created_at)
+# print(obj.updated_at)
+# print(obj)
