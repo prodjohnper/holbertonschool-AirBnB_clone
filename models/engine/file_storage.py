09853bda516a3601ac72472662ee844ce805f1fc
Louis Toro <louistororosario@gmail.com>
2024-02-20 21:29:33 -0400

    change self. to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,19 +14,19 @@ class FileStorage:
     __objects = {}
 
     def all(self): #Returns all stored objects.
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj): #Adds a new object to the internal dictionary of objects.
         name = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[name] = obj.to_dict()
+        FileStorage.__objects[name] = obj.to_dict()
 
     def save(self): #Saves the current state of objects to the JSON file.
         with open(self.__file_path, "w") as file:
-            json.dump(self.__objects, file)
+            json.dump(FileStorage.__objects, file)
 
     def reload(self): #Reloads objects from the JSON file.
         try:
             with open(self.__file_path, "r") as file:
-                self.__objects = json.load(file)
+                FileStorage.__objects = json.load(file)
         except FileNotFoundError:
             pass
