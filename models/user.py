79de0372ff9270b34432c6650ac016cafc6cea97
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-21 23:55:03 -0400

    Fix - Implemented str representation of User object

--- models/user.py
+++ models/user.py
@@ -6,8 +6,16 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-
+    '''
+        User class
+    '''
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __str__(self):
+        '''
+            String representation of User object
+        '''
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
