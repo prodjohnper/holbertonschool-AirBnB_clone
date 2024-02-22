15b6a35405fff3c7c5b8e8266d98272342d492ef
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-21 23:48:27 -0400

    Update - Implemented use of User into the console

--- console.py
+++ console.py
@@ -6,7 +6,7 @@
 '''
 import cmd
 import json
-from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
@@ -44,18 +44,21 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         '''
-            Creates a new instance of BaseModel
+            Creates a new instance of User
         '''
-        if arg == '':
+        args = arg.split()
+        if not args:
             print("** class name missing **")
             return
+        class_name = args[0]
 
-        try:
-            obj = eval(arg)()
-            obj.save()
-            print(obj.id)
-        except NameError:
+        if class_name != "User":
             print("** class doesn't exist **")
+            return
+
+        new_user = User()
+        new_user.save()
+        print(new_user.id)
 
     def all(self):
         '''
@@ -71,134 +74,130 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         '''
-            Prints the string representation of an instance
+            Prints the string representation of a User instance
         '''
         args = arg.split()
-        if len(args) == 0:
+        if not args:
             print("** class name missing **")
             return
-        if args[0] != "BaseModel":
-            if args[0] != "User":
-                print("** class doesn't exist **")
-                return
-        if len(args) == 1:
+        class_name = args[0]
+        if class_name != "User":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
             print("** instance id missing **")
             return
 
-        # Load data from JSON file
-        try:
-            with open("file.json", "r") as file:
-                data = json.load(file)
-        except FileNotFoundError:
+        user_id = args[1]
+        objs = self.all()
+        key = class_name + "." + user_id
+        if key not in objs:
             print("** no instance found **")
             return
 
-        key = args[0] + "." + args[1]
-        if key not in data:
-            print("** no instance found **")
-        else:
-            instance_dict = data[key]
-            instance = BaseModel(**instance_dict)
-            print(instance)
+        print(objs[key])
 
     def do_destroy(self, arg):
         '''
-            Deletes an instance based on the class name and id
+            Deletes a User instance based on the id
         '''
         args = arg.split()
-        if len(args) == 0:
+        if not args:
             print("** class name missing **")
             return
-        if args[0] != "BaseModel":
-            if args[0] != "User":
-                print("** class doesn't exist **")
-                return
-        if len(args) == 1:
+        class_name = args[0]
+        if class_name != "User":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
             print("** instance id missing **")
             return
+
+        user_id = args[1]
         objs = self.all()
-        key = args[0] + "." + args[1]
+        key = class_name + "." + user_id
         if key not in objs:
             print("** no instance found **")
-        else:
-            del objs[key]
-            with open('file.json', 'w') as file:
-                json.dump(objs, file)
+            return
+
+        del objs[key]
+        with open('file.json', 'w') as file:
+            json.dump(objs, file)
 
     def do_all(self, arg):
+        '''
+            Prints all string representations of User instances
+        '''
         if not arg:  # If no argument provided, print all instances
             with open('file.json', 'r') as file:
-                my_dict_from_json = json.load(file)
-                for key, dictionary in my_dict_from_json.items():
+                objs_dict = json.load(file)
+                for key, dictionary in objs_dict.items():
                     class_name, obj_id = key.split('.')
-                    obj = eval(class_name)(**dictionary)
-                    print("[{}] {}".format(obj_id, obj))
+                    if class_name == "User":
+                        obj = User(**dictionary)
+                        print(obj)
         else:
             args = arg.split()
             if len(args) == 1:
                 class_name = args[0]
-                if class_name in ["BaseModel", "User"]:
+                if class_name == "User":
                     with open('file.json', 'r') as file:
-                        my_dict_from_json = json.load(file)
-                        for key, dictionary in my_dict_from_json.items():
+                        objs_dict = json.load(file)
+                        for key, dictionary in objs_dict.items():
                             if key.startswith(class_name):
                                 obj_id = key.split('.')[1]
-                                obj = eval(class_name)(**dictionary)
-                                print("[{}] {}".format(obj_id, obj))
+                                obj = User(**dictionary)
+                                print(obj)
                 else:
                     print("** class doesn't exist **")
-        """ objs = self.all() """
 
     def do_update(self, arg):
+        '''
+        Updates an instance based on the class name and id
+        '''
         args = arg.split()
-        args = args[:4]  # Ensure only first 4 arguments are used
-
-        """ if len(args) > 4:
-            return """
-
-        if len(args) == 0:
-            print('** class name missing **')
+        if len(args) < 2:
+            print("** class name missing **")
             return
         class_name = args[0]
-
-        if class_name != "BaseModel":
-            if args[0] != "User":
-                print("** class doesn't exist **")
-                return
-
-        if len(args) < 2:
-            print('** instance id missing **')
+        if class_name != "User":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 3:
+            print("** instance id missing **")
             return
         obj_id = args[1]
 
+        # Load the objects from the JSON file
         try:
-            with open("file.json", "r") as file:
-                data = json.load(file)
+            with open('file.json', 'r') as file:
+                objs = json.load(file)
         except FileNotFoundError:
-            print("** no instance found **")
-            return
+            objs = {}
 
         key = f"{class_name}.{obj_id}"
-        if key not in data:
+        if key not in objs:
             print("** no instance found **")
             return
 
-        if len(args) < 3:
+        # Check if the object is an instance of User
+        if objs[key]["__class__"] != "User":
+            print("** Invalid instance **")
+            return
+
+        # Check if attribute name and value are provided
+        if len(args) < 4:
             print("** attribute name missing **")
             return
         attr_name = args[2]
+        attr_value = args[3]
 
-        if len(args) < 4:
-            print('** value missing **')
-            return
+        # Update attribute if it exists
+        objs[key][attr_name] = attr_value
 
-        attr_value = " ".join(args[3:])  # Handle attribute value with spaces
-
-        obj_dict = data[key]
-        obj_dict[attr_name] = attr_value  # Update attribute in the dictionary
-
-        with open("file.json", "w") as file:
-            json.dump(data, file)
+        # Save the changes back to the JSON file
+        with open('file.json', 'w') as file:
+            json.dump(objs, file)
 
 
 if __name__ == '__main__':
